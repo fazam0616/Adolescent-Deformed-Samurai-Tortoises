9287684f3a087d22457b13ae39aa744fad0e43d7
Fahim Zaman <fazam0616@hotmail.com>
2020-12-01 17:23:06 -0500

    Initial creation of basic classes

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import pygame
 
-#HI GUYS
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return str(self.x)+", "+str(self.y)
+
+
+class Character:
+    health = 100
+    def __init__(self, *point):
+        if len(point) == 2:
+            self.pos = Point(point[0], point[1])
+        else:
+            self.pos = point[0]
+
+a = Character(1,1)
+c = Character(Point(2,2))
+
+print(a.pos)
+print(c)
