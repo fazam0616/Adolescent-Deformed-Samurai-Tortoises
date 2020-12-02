093c9521fe22d393fb4ced6a01aaf6fa2887f6e5
Alx2003 <alexanderqlay@gmail.com>
2020-12-01 22:22:50 -0500

    tkinter stuff

--- Menu.py
+++ Menu.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import *
 
 # defines functions for buttons
 def playGame():
@@ -11,6 +12,12 @@ app.geometry("1280x600+25-100")
 
 # play button
 playButton = tkinter.Button(app, text="PLAY", bg="#006400", fg="#F5F5F5", command=playGame)
+playButton.config(height=3, width=20)
 playButton.pack()
+
+# character button
+characterButton = tkinter.Button(app, text="CHARACTER SELECT", bg="#006400", fg="#F5F5F5", command=playGame)
+characterButton.config(height=3, width=20)
+characterButton.pack()
 
 app.mainloop()
