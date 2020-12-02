10a9148b5235a39ee9a6abfa556958997a18790e
Alx2003 <alexanderqlay@gmail.com>
2020-12-01 21:46:16 -0500

    tkinter stuff

--- Menu.py
+++ Menu.py
@@ -1,9 +1,16 @@
 import tkinter
-from tkinter import *
 
+# defines functions for buttons
+def playGame():
+    print("plays game")
+
+# open window for application
 app = tkinter.Tk()
 app.title('Adolescent Deformed Samurai Tortoises')
-app.geometry("500x500+400+100")
+app.geometry("1280x600+25-100")
 
+# play button
+playButton = tkinter.Button(app, text="PLAY", bg="#006400", fg="#F5F5F5", command=playGame)
+playButton.pack()
 
 app.mainloop()
