4493b6986d2664587b96a41c1c1f6b6d5670bf60
Fahim Zaman <fazam0616@hotmail.com>
2020-12-02 19:25:51 -0500

    renamed folders for standardization

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ def main(*args):
     clock = pygame.time.Clock()
     wallimage = imageio.imread("images\\walls.bmp")
     wallMap = []
+    bg = pygame.image.load("images\\world.png")
 
-    player = PlayableCharacters.Leo(wallMap, Point(100,100))
+    player = PlayableCharacters.Leo(wallMap, Point(350,350))
     UP = False
     DOWN = False
     RIGHT = False
@@ -34,8 +35,10 @@ def main(*args):
     # Reading the wall map and creating a local version for look-up
     for x in range(0, 1250, 50):
         for y in range(0, 1250, 50):
-            wallMap[int(x / 50)-1][int(y / 50)-1] = True if wallimage[x][y][0] != 0 else False
+            wallMap[int(x / 50)][int(y / 50)] = True if wallimage[y][x][0] == 64 else False
 
+    for i in range(25):
+        print(wallMap[i])
     #Game loop
     while True:
         for event in pygame.event.get():
@@ -72,7 +75,6 @@ def main(*args):
                     RIGHT = False
                 if event.key == pygame.K_a:
                     LEFT = False
-
         if UP:
             player.move(Point(0,-5))
         elif DOWN:
@@ -81,8 +83,8 @@ def main(*args):
             player.move(Point(5,0))
         elif LEFT:
             player.move(Point(-5,0))
-
         screen.fill((0,0,0))
+        screen.blit(bg, (0, 0))
         screen.blit(player.getImage(),player.getPos())
         pygame.display.update()
         clock.tick(20)
