0851d63ab54dd58ee359abc1fd565e6bac2b11b8
Alx2003 <alexanderqlay@gmail.com>
2020-12-02 22:01:47 -0500

    menus

--- Menu.py
+++ Menu.py
@@ -16,23 +16,29 @@ def about():
     currentChar.place_forget()
     title.place(x=105, y=50)
 
-    aboutText = Label(app, text="Vector Map - Hunter Britton\n"
-                                "Menus - Alexander Lay\n"
-                                "Animations - Noah Nogueira\n"
-                                "Controls - Fahim Zaman", bg="#008000", fg="#F5F5F5", font=('Arial', 24))
-    aboutText.place(x=450, y=250)
-
-    backButton = Button(app, text="BACK", bg="#006400", fg="#F5F5F5", command=back)
-    backButton.config(height=3, width=20)
+    aboutText.place(x=390, y=200)
     backButton.place(x=560, y=500)
 
 def back():
-    main
+    title.place(x=105, y=100)
+    playButton.place(x=560, y=350)
+    characterButton.place(x=560, y=425)
+    aboutButton.place(x=560, y=500)
+    currentChar.place(x=50, y=400)
+    aboutText.place_forget()
+    backButton.place_forget()
 
 # open window for application
 app = Tk()
 app.title('Adolescent Deformed Samurai Tortoises')
 app.geometry("1280x600+30-100")
+aboutText = Label(app, text="AI Pathing - Hunter Britton\n"
+                                "Menus - Alexander Lay\n"
+                                "Animation Engine - Noah Nogueira\n"
+                                "Game Engine - Fahim Zaman\n\n"
+                                "THANKS FOR PLAYING!", bg="#008000", fg="#F5F5F5", font=('Arial', 24))
+backButton = Button(app, text="BACK", bg="#006400", fg="#F5F5F5", command=back)
+backButton.config(height=3, width=20)
 
 # title label
 title = Label(app, text="Adolescent Deformed Samurai Tortoises", bg="#008000", fg="#F5F5F5", font= ('Comic Sans MS',42))
