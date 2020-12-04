e7ea110b05843a54b9ab5860601c13ebf36664f9
huuunterr <hunterbritton321@yahoo.ca>
2020-12-03 20:59:51 -0500

    possibly works - Hunter

--- Enemy.py
+++ Enemy.py
@@ -3,12 +3,13 @@ import Character
 
 
 #here begins hunters shit code
-class Enemy (Character):
+class Enemy (): #perhaps a character arg might be used... idk man im tired
 
-    def __init__(self):
-        global bestDir = 'N'
-
-        global bestDirArray = [
+    def __init__(self, wallMap1): #accepts player position in reference to 0,0 being top left
+        bestDir = 'N'
+        global wallMap
+        wallMap = wallMap1
+        bestDirArray = [
             ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
              '0', '0', '0'],
             ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
@@ -66,98 +67,114 @@ class Enemy (Character):
                     bestDirArray[row][column] = None
                 else:
                     bestDirArray[row][column] = 'N'
+        self.bestDirArray=bestDirArray
+
+        self.x
+        self.y
+
+        distToPlayer = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 25x25
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 1,1 is first playable spot
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # if x or y is 0 or 25 it is out of bounds
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+        for row in range(25):  # fills the array with a high value for the setDir to ignore unless it has been changed
+            for column in range(25):
+                distToPlayer[column][row] = 999999
+        self.distToPlayer = distToPlayer
 
 
-    def setDirection(self, x, y, distToPlayer): # takes the cordinates and sets a direction to the best next move... hunter doo doo code.. idrk where it goes...
+    def setDirection(self): # takes the cordinates and sets a direction to the best next move... hunter doo doo code.. idrk where it goes...
 
-        self.bestDist=distToPlayer[x][y-1] #checks the best distance in a square around it and sets the direction to move there
+        self.bestDist=self.distToPlayer[self.x][self.y-1] #checks the best distance in a square around it and sets the direction to move there
         self.bestDir='N'
-        tempX= x
-        tempY= y - 1
+        tempX= self.x
+        tempY= self.y - 1
 
-        if ((distToPlayer[x+1][y]) < self.bestDist):
-            self.bestDist=distToPlayer[x+1][y]
+        if ((self.distToPlayer[self.x+1][self.y]) < self.bestDist):
+            self.bestDist=self.distToPlayer[self.x+1][self.y]
             self.bestDir = 'E'
-            tempX = x + 1
-            tempY = y
+            tempX = self.x + 1
+            tempY = self.y
 
-        if ((distToPlayer[x][y+1]) < self.bestDist):
-            self.bestDist=distToPlayer[x][y+1]
+        if ((self.distToPlayer[self.x][self.y+1]) < self.bestDist):
+            self.bestDist=self.distToPlayer[self.x][self.y+1]
             self.bestDir = 'S'
-            tempX = x
-            tempY = y + 1
+            tempX = self.x
+            tempY = self.y + 1
 
-        if ((distToPlayer[x-1][y]) < self.bestDist):
-            self.bestDist=distToPlayer[x-1][y]
+        if ((self.distToPlayer[self.x-1][self.y]) < self.bestDist):
+            self.bestDist=self.distToPlayer[self.x-1][self.y]
             self.bestDir = 'W'
-            tempX = x - 1
-            tempY = y
+            tempX = self.x - 1
+            tempY = self.y
 
         self.bestDirArray[tempX][tempY]=self.bestDir  # sets the cords of the best spot to the array
         return self.bestDist
 
-    def setDist(self, x, y, wallMap): #accepts player position in reference to 0,0 being top left
-        distToPlayer = [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 25x25
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 1,1 is first playable spot
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # if x or y is 0 or 25 it is out of bounds
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        ]
-        for row in range(25):  # fills the array with a high value for the setDir to ignore unless it has been changed
-            for column in range(25):
-                self.distToPlayer[column][row] = 999999
+    def setDist(self):
 
-        self.distToPlayer[x][y]=0
+
+        self.distToPlayer[self.x][self.y]=0
 
         def spiralCheck():
-            if not(self.wallMap[x][y]): #starts the spiral to check squares
-                self.distToPlayer[x][y]= self.setDirection(x,y,distToPlayer) + 1 #set a pos in the array to the best direction and just so happens to return a value for distance to player
+            if not(self.wallMap[self.x][self.y]): #starts the spiral to check squares
+                self.distToPlayer[self.x][self.y]= self.setDirection(self.x,self.y) + 1 #set a pos in the array to the best direction and just so happens to return a value for distance to player
 
-        OOB_count=0 # out of bounds count
-        while(self.OOB_count < 25): # makes it go i na spiral around the distance map
+        self.OOB_count = 0 # out of bounds count
+        while(self.OOB_count < 15): # makes it go i na spiral around the distance map
             self.x += 1
-            while distToPlayer[x][y-1] < 9999 and not wallMap[x][y-1]:
+            while self.distToPlayer[x][y-1] < 9999 and not wallMap[self.x][self.y-1]:
+                spiralCheck()
                 self.x += 1
                 self.OOB_count=0
             self.y -= 1
-            while distToPlayer[x-1][y] < 9999 and not wallMap[x-1][y]:
+            while self.distToPlayer[self.x-1][self.y] < 9999 and not wallMap[self.x-1][self.y]:
+                spiralCheck()
                 self.y -= 1
                 self.OOB_count = 0
             self.x -= 1
-            while distToPlayer[x][y+1] < 9999 and not wallMap[x][y+1]:
+            while self.distToPlayer[self.x][self.y+1] < 9999 and not wallMap[self.x][self.y+1]:
+                spiralCheck()
                 self.x -= 1
                 self.OOB_count = 0
             self.y += 1
-            while distToPlayer[x+1][y] < 9999 and not wallMap[x+1][y]:
+            while self.distToPlayer[self.x+1][self.y] < 9999 and not wallMap[self.x+1][self.y]:
+                spiralCheck()
                 self.y += 1
                 self.OOB_count = 0
 
             self.OOB_count +=1
 
-    def repath:
+    def repath(self, x, y): #accepts player position in reference to 0,0 being top left
+        self.x = x
+        self.y = y
+        Enemy.setDist()
 
 
-    def getVectorField(wallMap):
-       return bestDirArray
+    def getVectorField(self):
+       return self.bestDirArray
+
+
