c924fe4c03ae416d15cb500f5da1447f20d6dc71
Noah <chimpydimpa@gmail.com>
2021-01-17 21:12:37 -0500

    starting pizza

---
+++ Item.py
@@ -0,0 +1,16 @@
+import pygame
+
+import Character
+
+class Item:
+    def __init__(self):
+        self.baseImage = None
+        self.pos = point
+
+    def imageSetup(self, name):
+        self.baseImage = pygame.image.load("images\\"+name+"\\"+name+".png")
+
+
+
+
+
---
+++ Pizza.py
@@ -0,0 +1,20 @@
+import imageio
+import pygame
+
+import Item
+
+class Health():
+    def __init__(self):
+        super().__init__()
+        self.imageSetup("health")
+
+
+class Speed(Character.Character):
+    def __init__():
+        super().imageSetup("speed")
+
+
+class Damage(Character.Character):
+    def __init__():
+        super().imageSetup("damage")
+
